a963540bfd4d456665c6d1005af753591c07c4d8
Mikdiet <mikdiet@gmail.com>
2014-03-01 21:58:14 +0300

    spec for Repo.find_or_initialize_from_github

---
+++ spec/factories/repo.rb
@@ -0,0 +1,5 @@
+FactoryGirl.define do
+  factory :repo do
+    name 'test/test'
+  end
+end
--- spec/models/star_spec.rb
+++ spec/models/star_spec.rb
@@ -1,5 +1,31 @@
 require 'spec_helper'
 
 describe Star do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '.find_or_initialize_from_github' do
+    let!(:repo){ create :repo, name: 'user/repo' }
+    let!(:star){ create :star, repo: repo }
+
+    it 'creates new repo' do
+      data = double full_name: 'another/another', description: 'foo bar'
+      expect{ Star.find_or_initialize_from_github(data) }.to change{ Repo.count }.by(1)
+      expect( Repo.last.name ).to eq 'another/another'
+      expect( Repo.last.description ).to eq 'foo bar'
+    end
+
+    it 'updates existed repo' do
+      data = double full_name: 'user/repo', description: 'foo bar'
+      expect{ Star.find_or_initialize_from_github(data) }.to_not change{ Repo.count }
+      expect( repo.reload.description ).to eq 'foo bar'
+    end
+
+    it 'returns existed star' do
+      data = double full_name: 'user/repo', description: 'foo bar'
+      expect( Star.find_or_initialize_from_github(data) ).to eq star
+    end
+
+    it 'initializes new star' do
+      data = double full_name: 'another/another', description: 'foo bar'
+      expect( Star.find_or_initialize_from_github(data) ).to be_new_record
+    end
+  end
 end
